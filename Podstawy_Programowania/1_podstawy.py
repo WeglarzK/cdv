890b1e9b187f568fa57f503d3e817831931ce334
WeglarzK <starcraft27h@gmail.com>
2020-03-29 14:11:04 +0200

    new

--- Podstawy_Programowania/1_podstawy.py
+++ Podstawy_Programowania/1_podstawy.py
@@ -1,2 +1,26 @@
-test
-test
+print("CDV11")
+print(8)
+
+#potęgowanie
+#pow = 2**10
+#print(pow)
+
+#pobieranie danych z klawiatury
+#name = input()
+#print("Twoje imie: "+name)
+
+#surname=input()
+#print("Twoje imie: " + name + ", nazwisko: " + surname)
+#length= len(surname)
+#print(length)
+#print(type(surname))
+#print(type(length))
+
+#length = str(length)
+#print(type(length))
+
+name1=input()
+surname1=input()
+age=input()
+
+print("Twoje imie: " + name1 + "\nTwoje nazwisko: " + surname1 + "\nTwoj wiek: " + age +".")
